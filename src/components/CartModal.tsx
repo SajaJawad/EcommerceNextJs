d19cc0d200ad5613ac40936f3ddf1dd416947f0e
SajaJawad <saiamaher2002@gmail.com>
2025-11-22 11:33:09 +0200

    Create Navbar , Header , NavIcons

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Image from "next/image"
+
+const CartModal = () => {
+
+
+  const cartItems = true
+
+  return (
+    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {
+        !cartItems ? (
+          <div className="">Cart is Empty</div>
+        ) : (
+          <div className="">
+            <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" width={72} height={96} className="object-cover rounded-md" />
+
+            <div className="">
+              {/* TOP */}
+              <div className="">
+                {/*TITLE */}
+                <div className="">
+                  <h3>Product Name</h3>
+                  <div>$98</div>
+                </div>
+                {/*DESC */}
+
+                <div className="">
+                  available
+                </div>
+
+              </div>
+
+              {/* BOTTOM */}
+
+            </div>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default CartModal
