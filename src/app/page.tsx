cb4bab0a9bb138a85c08483372321168b42b8d11
SajaJawad <saiamaher2002@gmail.com>
2025-11-23 13:40:35 +0200

    Create Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,18 @@ import SlideBar from "@/components/SlideBar"
 const HomePage = () => {
   return (
     <div className=''>
-      <SlideBar/>
+      <SlideBar />
       <div className="mt-24  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList/>
+        <ProductList />
       </div>
-      <div className="mt-24  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
-        <h1 className="text-2xl">Categories</h1>
-        <CategoryList/>
+      <div className="mt-24  ">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">Categories</h1>
+        <CategoryList />
       </div>
       <div className="mt-24  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <NewProducts/>
+        <NewProducts />
       </div>
     </div>
   )
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -5,14 +5,14 @@ import Link from "next/link"
 
 const ProductList = () => {
     return (
-       
 
- <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap ">
+
+        <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap ">
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
 
                 <div className="relative w-full h-80 ">
-                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"/>
-                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md"/>
+                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md" />
                 </div>
                 <div className="flex justify-between ">
                     <span className="font-medium">Product Name</span>
@@ -24,8 +24,8 @@ const ProductList = () => {
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
 
                 <div className="relative w-full h-80 ">
-                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"/>
-                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md"/>
+                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md" />
                 </div>
                 <div className="flex justify-between ">
                     <span className="font-medium">Product Name</span>
@@ -37,8 +37,8 @@ const ProductList = () => {
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
 
                 <div className="relative w-full h-80 ">
-                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"/>
-                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md"/>
+                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md" />
                 </div>
                 <div className="flex justify-between ">
                     <span className="font-medium">Product Name</span>
@@ -50,8 +50,8 @@ const ProductList = () => {
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
 
                 <div className="relative w-full h-80 ">
-                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"/>
-                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md"/>
+                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md" />
                 </div>
                 <div className="flex justify-between ">
                     <span className="font-medium">Product Name</span>
@@ -63,8 +63,8 @@ const ProductList = () => {
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
 
                 <div className="relative w-full h-80 ">
-                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"/>
-                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md"/>
+                    <Image src="/pexels-hammad-khalid-155549-1786433.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                    <Image src="/pexels-jeremy-bishop-1260133-2765873.jpg" alt="" fill sizes="25vw" className="absolute object-cover rounded-md" />
                 </div>
                 <div className="flex justify-between ">
                     <span className="font-medium">Product Name</span>
